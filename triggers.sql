487ef1cf511f8025ca6073d674fca85df40bd115
frojaskey <f.rojaskey@gmail.com>
2016-08-22 15:20:36 -0400

    insert trigger

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,12 @@
-trigger
+CREATE OR REPLACE TRIGGER ref_estadio
+BEFORE INSERT OF estadio
+ON equipo
+BEGIN
+SELECT equipo
+FROM estadio e
+WHERE e.nombre = :NEW.estadio
+IF NOT(e.quipo = NULL) THEN
+RAISE_APPLICATION_ERROR(-20505,'El estadio ya tiene equipo');
+END IF;
+END;
+/
