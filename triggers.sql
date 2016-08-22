cdd9dac46a0053bb15c7494f9e1789a0b5911908
frojaskey <f.rojaskey@gmail.com>
2016-08-22 16:36:13 -0400

    mod22

--- triggers.sql
+++ triggers.sql
@@ -1,14 +1,16 @@
 CREATE OR REPLACE TRIGGER ref_estadio
 BEFORE INSERT ON equipo
-SELECT equipo
-FROM estadio e
-WHERE e.nombre = :NEW.estadio
-IF NOT(e.quipo = NULL) THEN
-RAISE_APPLICATION_ERROR(-20505,'El estadio ya tiene equipo');
-END IF;
+BEGIN
+	for reg in (select * from ESTADIO e where e.nombre = :NEW.estadio) loop 
+ 	IF NOT (reg.equipo == NULL) THEN
+	RAISE_APPLICATION_ERROR(-20505,'El estadio ya tiene equipo');
+	END IF;
+ 	end loop;
 END;
 /
-CREATE TRIGGER estadio_delete AFTER DELETE on equipo
+
+CREATE OR REPLACE TRIGGER estadio_delete 
+AFTER DELETE ON equipo
 BEGIN
 	for reg in (select * from ESTADIO where DEREF(equipo).nombre=:old.nombre) loop 
  	SET reg.estadio= NULL
