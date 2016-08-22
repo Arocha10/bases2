4dcd7e4a591ddbdd72dd3d1c6391b2838cf8d9f8
frojaskey <f.rojaskey@gmail.com>
2016-08-22 16:56:51 -0400

    ordenando un pelo esto

---
+++ drop_tables.sql
@@ -0,0 +1,17 @@
+/* 
+Andres Rocha 12-11247
+Francisco Rojas 12-10515
+*/
+DROP TABLE estadio CASCADE CONSTRAINTS;
+DROP TYPE estadio_t force;
+DROP TYPE patr_tab force;
+DROP TABLE ganador;
+DROP TYPE ganador_t;
+DROP TABLE titulo;
+DROP TYPE titulo_t;
+DROP TABLE integrante;
+DROP TYPE jugador_t force;
+DROP TYPE entrenador_t;
+DROP TYPE integrante_t force;
+DROP TABLE equipo CASCADE CONSTRAINTS;
+DROP TYPE equipo_t force;
---
+++ drop_trigger.sql
@@ -0,0 +1,2 @@
+DROP TRIGGER ref_estadio;
+DROP TRIGGER estadio_delete;
